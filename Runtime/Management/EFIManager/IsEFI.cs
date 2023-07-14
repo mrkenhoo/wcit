847da96de185d71e06a2ed3ca3825d6b25bafd25
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2023-07-14 13:57:13 +0100

    IsEFI: restore the old code
    It was not working as expected

--- Runtime/Management/EFIManager/IsEFI.cs
+++ Runtime/Management/EFIManager/IsEFI.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.Win32;
+﻿using System.Runtime.InteropServices;
 
 namespace Runtime.Management.EFIManager
 {
     static partial class GetEFIInfo
     {
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetFirmwareEnvironmentVariableA",
+            SetLastError = true,
+            CharSet = CharSet.Unicode,
+            ExactSpelling = true,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetFirmwareType(string lpName, string lpGUID, IntPtr pBuffer, uint size);
+
         internal static bool IsEFI()
         {
-            const string UefiRegistryKeyPath = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control";
-            const string UefiRegistryValueName = "FirmwareEnvironmentVariable";
+            // Call the function with a dummy variable name and a dummy variable namespace (function will fail because these don't exist.)
+            GetFirmwareType("", "{00000000-0000-0000-0000-000000000000}", IntPtr.Zero, 0);
 
-            object firmwareEnvironmentVariable = Registry.GetValue(UefiRegistryKeyPath, UefiRegistryValueName, null);
+            if (Marshal.GetLastWin32Error() == 0)
+            {
+                return true;
+            }
 
-            return firmwareEnvironmentVariable != null;
+            return true;
         }
     }
 }
