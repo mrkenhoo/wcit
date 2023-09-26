a2ce56411604a214815b09cefd333d353156c86d
Ken Hoo <15352345-mrkenhoo@users.noreply.gitlab.com>
2023-09-26 16:54:24 +0100

    Make sure EfiDrive and DestinationDrive\windows is not already in use

--- Runtime/Utilities/Deployment/InstallBootloader.cs
+++ Runtime/Utilities/Deployment/InstallBootloader.cs
@@ -8,7 +8,18 @@ namespace Runtime.Utilities.Deployment
         {
             try
             {
-                Worker.StartCmdProcess("bcdboot", $"{DestinationDrive}\\windows /s {EfiDrive} /f {FirmwareType}");
+                if (Directory.Exists(EfiDrive))
+                {
+                    throw new Exception("The drive letter " + EfiDrive + "is already in use.");
+                }
+                else if (!Directory.Exists($"{DestinationDrive}\\windows"))
+                {
+                    throw new DirectoryNotFoundException($"Could not find the following directory: {DestinationDrive}\\windows");
+                }
+                else
+                {
+                    Worker.StartCmdProcess("bcdboot", $"{DestinationDrive}\\windows /s {EfiDrive} /f {FirmwareType}");
+                }
             }
             catch (Exception)
             {
