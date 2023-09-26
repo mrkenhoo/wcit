7b829746288ec6bd04ed4260bb0a6467a5a6fe01
Ken Hoo <15352345-mrkenhoo@users.noreply.gitlab.com>
2023-09-26 17:46:06 +0100

    ApplyImage: dont' deploy Windows if it is found
    Check if the arguments are null and throw an exception. Also check if Windows seems to be already installed and skip the installation.

--- Runtime/Utilities/Deployment/ApplyImage.cs
+++ Runtime/Utilities/Deployment/ApplyImage.cs
@@ -6,19 +6,39 @@ namespace Runtime.Utilities.Deployment
     {
         public static void ApplyImage(string SourceDrive, string DestinationDrive, int Index)
         {
+            if (SourceDrive == null)
+            {
+                throw new ArgumentNullException(nameof(SourceDrive));
+            }
+            else if (DestinationDrive == null)
+            {
+                throw new ArgumentNullException(nameof(DestinationDrive));
+            }
+            if (Index <= 0)
+            {
+                throw new ArgumentException("No Windows edition was chosen", nameof(Index));
+            }
+
             try
             {
-                if (File.Exists($"{SourceDrive}\\sources\\install.esd"))
+                if (!Directory.Exists(DestinationDrive + "\\windows"))
                 {
-                    Worker.StartDismProcess($"/apply-image /imagefile:{SourceDrive}\\sources\\install.esd /applydir:{DestinationDrive}\\ /index:{Index} /verify");
-                }
-                else if (File.Exists($"{SourceDrive}\\sources\\install.wim"))
-                {
-                    Worker.StartDismProcess($"/apply-image /imagefile:{SourceDrive}\\sources\\install.wim /applydir:{DestinationDrive}\\ /index:{Index} /verify");
+                    if (File.Exists($"{SourceDrive}\\sources\\install.esd"))
+                    {
+                        Worker.StartDismProcess($"/apply-image /imagefile:{SourceDrive}\\sources\\install.esd /applydir:{DestinationDrive}\\ /index:{Index} /verify");
+                    }
+                    else if (File.Exists($"{SourceDrive}\\sources\\install.wim"))
+                    {
+                        Worker.StartDismProcess($"/apply-image /imagefile:{SourceDrive}\\sources\\install.wim /applydir:{DestinationDrive}\\ /index:{Index} /verify");
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException("Could not find a valid image");
+                    }
                 }
                 else
                 {
-                    throw new FileNotFoundException("Could not find a valid image");
+                    Console.Error.WriteLine("Windows seems to be already deployed, not overwriting it.");
                 }
             }
             catch (Exception)
