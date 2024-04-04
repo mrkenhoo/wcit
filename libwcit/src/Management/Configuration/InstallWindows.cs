0c81063eeba6291848c31f5713bce211786ecb6a
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-04-04 18:45:32 +0100

    libwcit: improve function args handling

--- libwcit/src/Management/Configuration/InstallWindows.cs
+++ libwcit/src/Management/Configuration/InstallWindows.cs
@@ -6,36 +6,24 @@ namespace libwcit.Management.Installer
 {
     public partial class Configuration
     {
-        public static void InstallWindows(int? DiskNumber, string? DestinationDrive, string? EfiDrive, string? ImageFile, int? WindowsEdition)
+        public static void InstallWindows(int DiskNumber, string DestinationDrive, string EfiDrive, string ImageFile, int WindowsEdition)
         {
             try
             {
-                if (DiskNumber == null)
+                if (DiskNumber < 0)
                 {
-                    throw new ArgumentNullException(nameof(DiskNumber), $"{nameof(DiskNumber)} cannot be null");
-                }
-                else if (DiskNumber < 0)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(DiskNumber), $"{nameof(DiskNumber)} cannot be null");
-                }
-                ArgumentException.ThrowIfNullOrWhiteSpace(nameof(DestinationDrive));
-                ArgumentException.ThrowIfNullOrWhiteSpace(nameof(EfiDrive));
-                ArgumentException.ThrowIfNullOrWhiteSpace(nameof(ImageFile));
-
-                if (WindowsEdition == null)
-                {
-                    throw new ArgumentNullException(nameof(WindowsEdition), $"{nameof(WindowsEdition)} cannot be null");
+                    throw new ArgumentOutOfRangeException(nameof(DiskNumber), $"{nameof(DiskNumber)} cannot be set to less than 0.");
                 }
                 else if (WindowsEdition < 0)
                 {
                     throw new ArgumentOutOfRangeException(nameof(WindowsEdition), $"{nameof(WindowsEdition)} cannot be null");
                 }
 
-                SystemDrives.FormatDisk((int)DiskNumber, DestinationDrive, EfiDrive);
+                SystemDrives.FormatDisk(DiskNumber, DestinationDrive, EfiDrive);
 
                 Console.WriteLine($"\nImage file: {NewDeploy.ImageFile}");
                 Console.WriteLine($"==> Deploying Windows to drive {DestinationDrive} in disk {DiskNumber}, please wait...");
-                NewDeploy.ApplyImage(ImageFile, DestinationDrive, (int)WindowsEdition);
+                NewDeploy.ApplyImage(ImageFile, DestinationDrive, WindowsEdition);
 
                 Console.WriteLine($"\n==> Installing bootloader to drive {EfiDrive} in disk {DiskNumber}");
                 NewDeploy.InstallBootloader(DestinationDrive, EfiDrive, "UEFI");
