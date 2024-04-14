1ad7970e23e69a00bc44fa215046b914d2dd0776
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-04-14 10:47:52 +0100

    Implement a new StartDiskpartProcess function
    Make the FormatDisk function to use the newly created StartDiskpartFunction. It is mostly a wrapper now with a friendly name.

--- WindowsInstallerLib/src/Management/DiskManagement/SystemDrives/FormatDisk.cs
+++ WindowsInstallerLib/src/Management/DiskManagement/SystemDrives/FormatDisk.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Runtime.Versioning;
+﻿using System.Runtime.Versioning;
 using WindowsInstallerLib.Management.ProcessManager;
 
 namespace WindowsInstallerLib.Management.DiskManagement
@@ -13,75 +10,11 @@ namespace WindowsInstallerLib.Management.DiskManagement
         {
             try
             {
-                using Process process = new();
-                process.StartInfo.FileName = "diskpart.exe";
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardInput = true;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.Start();
-
-                Console.WriteLine($"Selecting disk {DiskNumber}...");
-                process.StandardInput.WriteLine($"select disk {DiskNumber}");
-                
-                Console.WriteLine($"Wiping disk {DiskNumber}...");
-                process.StandardInput.WriteLine("clean");
-
-                Console.WriteLine($"Converting disk {DiskNumber} to GPT...");
-                process.StandardInput.WriteLine("convert gpt");
-
-                Console.WriteLine($"Creating EFI partition from disk {DiskNumber}...");
-                process.StandardInput.WriteLine("create partition efi size=100");
-
-                Console.WriteLine($"Formatting EFI partition from disk {DiskNumber}...");
-                process.StandardInput.WriteLine("format fs=fat32 quick");
-
-                Console.WriteLine($"Mounting EFI partition to {EfiDrive} from disk {DiskNumber}...");
-                process.StandardInput.WriteLine($"assign letter {EfiDrive}");
-
-                Console.WriteLine($"Creating MSR partition in disk {DiskNumber}...");
-                process.StandardInput.WriteLine("create partition msr size=16");
-
-                Console.WriteLine($"Creating primary partition in disk {DiskNumber}...");
-                process.StandardInput.WriteLine("create partition primary");
-
-                Console.WriteLine($"Formatting primary partition in disk {DiskNumber}...");
-                process.StandardInput.WriteLine("format fs=ntfs quick");
-
-                Console.WriteLine($"Mounting primary partition to {DestinationDrive} from disk {DiskNumber}...");
-                process.StandardInput.WriteLine($"assign letter {DestinationDrive}");
-
-                process.StandardInput.WriteLine("exit");
-
-                process.WaitForExit();
-                Worker.ExitCode = process.ExitCode;
-                process.Close();
-
-                switch (Worker.ExitCode)
-                {
-                    case 0:
-                        Console.WriteLine($"\nDisk {DiskNumber} has been formatted successfully.");
-                        break;
-                    case 1:
-                        Console.Error.WriteLine($"\nFailed to format the disk {DiskNumber}.");
-                        break;
-                }
+                Worker.StartDiskpartProcess(DiskNumber, EfiDrive, DestinationDrive);
 
                 return Worker.ExitCode;
             }
-            catch (ObjectDisposedException)
-            {
-                throw;
-            }
-            catch (InvalidOperationException)
-            {
-                throw;
-            }
-            catch (Win32Exception)
-            {
-                throw;
-            }
-            catch (SystemException)
+            catch
             {
                 throw;
             }
---
+++ WindowsInstallerLib/src/Management/ProcessManager/Worker/StartDiskpartProcess.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace WindowsInstallerLib.Management.ProcessManager
+{
+    static partial class Worker
+    {
+        internal static int StartDiskpartProcess(int DiskNumber, string EfiDrive, string DestinationDrive)
+        {
+            try
+            {
+                Process process = new();
+                process.StartInfo.FileName = "diskpart.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+
+                Console.WriteLine($"Selecting disk {DiskNumber}...");
+                process.StandardInput.WriteLine($"select disk {DiskNumber}");
+
+                Console.WriteLine($"Wiping disk {DiskNumber}...");
+                process.StandardInput.WriteLine("clean");
+
+                Console.WriteLine($"Converting disk {DiskNumber} to GPT...");
+                process.StandardInput.WriteLine("convert gpt");
+
+                Console.WriteLine($"Creating EFI partition from disk {DiskNumber}...");
+                process.StandardInput.WriteLine("create partition efi size=100");
+
+                Console.WriteLine($"Formatting EFI partition from disk {DiskNumber}...");
+                process.StandardInput.WriteLine("format fs=fat32 quick");
+
+                Console.WriteLine($"Mounting EFI partition to {EfiDrive} from disk {DiskNumber}...");
+                process.StandardInput.WriteLine($"assign letter {EfiDrive}");
+
+                Console.WriteLine($"Creating MSR partition in disk {DiskNumber}...");
+                process.StandardInput.WriteLine("create partition msr size=16");
+
+                Console.WriteLine($"Creating primary partition in disk {DiskNumber}...");
+                process.StandardInput.WriteLine("create partition primary");
+
+                Console.WriteLine($"Formatting primary partition in disk {DiskNumber}...");
+                process.StandardInput.WriteLine("format fs=ntfs quick");
+
+                Console.WriteLine($"Mounting primary partition to {DestinationDrive} from disk {DiskNumber}...");
+                process.StandardInput.WriteLine($"assign letter {DestinationDrive}");
+
+                process.StandardInput.WriteLine("exit");
+
+                process.WaitForExit();
+                ExitCode = process.ExitCode;
+                process.Close();
+
+                switch (ExitCode)
+                {
+                    case 0:
+                        Console.WriteLine($"\nDisk {DiskNumber} has been formatted successfully.");
+                        break;
+                    case 1:
+                        Console.Error.WriteLine($"\nFailed to format the disk {DiskNumber}.");
+                        break;
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            catch (Win32Exception)
+            {
+                throw;
+            }
+            catch (SystemException)
+            {
+                throw;
+            }
+
+            return ExitCode;
+        }
+    }
+}
