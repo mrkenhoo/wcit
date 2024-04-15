934833211ead16cd104b6d596165a015c6343bda
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-04-15 23:07:02 +0100

    Check por admin privileges before running AddDrivers

--- WindowsInstallerLib/src/Utilities/Deployment/AddDrivers.cs
+++ WindowsInstallerLib/src/Utilities/Deployment/AddDrivers.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.Versioning;
+using WindowsInstallerLib.Management.PrivilegesManager;
 using WindowsInstallerLib.Management.ProcessManager;
 
 namespace WindowsInstallerLib.Utilities.Deployment
@@ -13,14 +14,22 @@ namespace WindowsInstallerLib.Utilities.Deployment
         /// </summary>
         /// <param name="DestinationDrive"></param>
         /// <param name="DriversSource"></param>
-        public static void AddDrivers(string ImageFile, string DriversSource)
+        public static int AddDrivers(string ImageFile, string DriversSource)
         {
             try
             {
                 ArgumentException.ThrowIfNullOrWhiteSpace(ImageFile, nameof(ImageFile));
                 ArgumentException.ThrowIfNullOrWhiteSpace(DriversSource, nameof(DriversSource));
 
-                Worker.StartDismProcess($"/Image:{ImageFile} /Add-Driver /Drive:{DriversSource} /recurse");
+                switch (GetPrivileges.IsUserAdmin())
+                {
+                    case true:
+                        Worker.StartDismProcess($"/Image:{ImageFile} /Add-Driver /Drive:{DriversSource} /recurse");
+                        return Worker.ExitCode;
+                    case false:
+                        Worker.StartDismProcess($"/Image:{ImageFile} /Add-Driver /Drive:{DriversSource} /recurse", RunAsAdministrator: true);
+                        return Worker.ExitCode;
+                }
             }
             catch (Exception)
             {
