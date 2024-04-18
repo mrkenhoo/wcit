a4c97f0b8610ef3200b57bd31b6d3948b9d17b27
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-04-18 17:18:21 +0100

    Make ImageFile set propery private

--- WindowsInstallerLib/src/Utilities/Deployment/DeploymentFlags.cs
+++ WindowsInstallerLib/src/Utilities/Deployment/DeploymentFlags.cs
@@ -2,6 +2,6 @@ namespace WindowsInstallerLib.Utilities.Deployment
 {
     public partial class NewDeploy
     {
-        public static string? ImageFile { get; set; }
+        public static string? ImageFile { get; private set; }
     }
 }
