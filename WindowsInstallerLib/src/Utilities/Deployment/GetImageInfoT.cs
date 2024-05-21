26cb01d37988a4557b09417830f34e8f5682da3c
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-05-21 17:28:22 +0100

    WindowsInstallerLib: add new function GetImageInfoT

---
+++ WindowsInstallerLib/src/Utilities/Deployment/GetImageInfoT.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Dism;
+using System;
+using System.IO;
+using System.Runtime.Versioning;
+using WindowsInstallerLib.Management.PrivilegesManager;
+
+namespace WindowsInstallerLib.Utilities.Deployment
+{
+    [SupportedOSPlatform("windows")]
+    public partial class NewDeploy
+    {
+        /// <summary>
+        /// Gets all Windows editions available using DISM, if any.
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static DismImageInfoCollection GetImageInfoT(string ImageFilePath)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(ImageFilePath))
+                {
+                    throw new FileNotFoundException("No image file was specified.", ImageFilePath);
+                }
+
+                switch (GetPrivileges.IsUserAdmin())
+                {
+                    case true:
+                        DismApi.Initialize(DismLogLevel.LogErrorsWarnings);
+                        break;
+                    case false:
+                        throw new UnauthorizedAccessException("Cannot initialize the DISM API without Administrator privileges.");
+                }
+
+                DismImageInfoCollection images = DismApi.GetImageInfo(ImageFilePath);
+
+                return images;
+            }
+            catch(DismException)
+            {
+                throw;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                DismApi.Shutdown();
+            }
+        }
+    }
+}
