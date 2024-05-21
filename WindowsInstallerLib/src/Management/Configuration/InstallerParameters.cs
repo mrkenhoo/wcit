ad1c56422ae390b8dc1d67275aae5b0b5466eace
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-05-21 17:26:18 +0100

    WindowsInstallerLib: rewrite parameters file

---
+++ WindowsInstallerLib/src/Management/Configuration/InstallerParameters.cs
@@ -0,0 +1,14 @@
+namespace WindowsInstallerLib.Management.Installer
+{
+    public partial class NewInstallation
+    {
+        public static string DestinationDrive;
+        public static string EfiDrive;
+        public static int DiskNumber;
+        public static string SourceDrive;
+        public static int ImageIndex;
+        public static string ImageFilePath;
+        public static bool InstallExtraDrivers;
+        public static string FirmwareType;
+    }
+}
