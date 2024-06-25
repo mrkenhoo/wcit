4081cba330a8fde0819f5d115ae4386dd3b73039
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-06-25 12:19:15 +0100

    Move IsUserAdmin.cs to a new subfolder

---
+++ WindowsInstallerLib/src/Management/PrivilegesManager/GetPrivileges/IsUserAdmin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Principal;
+
+namespace WindowsInstallerLib.Management.PrivilegesManager
+{
+    [SupportedOSPlatform("windows")]
+    public class GetPrivileges
+    {
+        /// <summary>
+        /// Checks if the current user has Administrator privileges.
+        /// </summary>
+        /// <returns>true or false</returns>
+        public static bool IsUserAdmin()
+        {
+            try
+            {
+                WindowsPrincipal CurrentUser = new(WindowsIdentity.GetCurrent());
+                bool CurrentUserIsAdministrator = CurrentUser.IsInRole(WindowsBuiltInRole.Administrator);
+                return CurrentUserIsAdministrator;
+            }
+            catch(SecurityException)
+            {
+                throw;
+            }
+            catch(ArgumentException)
+            {
+                throw;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
